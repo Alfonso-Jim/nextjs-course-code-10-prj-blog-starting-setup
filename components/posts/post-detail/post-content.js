4adbe210463f98465c329763319a2b92d85a7b22
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-09-29 22:47:00 +0200

    Add PostContent component for displaying post details

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,22 @@
+import PostHeader from './post-header';
+import classes from './post-content.module.css';
+
+const DUMMY_POST = {
+  slug: 'getting-started-with-nextjs',
+  title: 'Getting Started with my webit000',
+  image: 'getting-started-nextjs.png',
+  content: '# Un ejemplo muy cagado de markdown',
+  date: '2022-02-10',
+};
+
+const PostContent = (props) => {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+};
+
+export default PostContent;
