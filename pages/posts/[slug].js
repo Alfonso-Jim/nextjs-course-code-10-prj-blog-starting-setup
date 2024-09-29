6ec3f77dbf9a34a5593cca87e20713bfcc1a6706
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-09-30 00:11:20 +0200

    Refactor [slug].js to include dynamic post data in PostDetailPage

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,29 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/post-utils';
 
 const PostDetailPage = (props) => {
-  return <PostContent />;
+  return <PostContent post={props.post} />;
+};
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = () => {
+  const postFilenames = getPostsFiles();
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
 };
 
 export default PostDetailPage;
