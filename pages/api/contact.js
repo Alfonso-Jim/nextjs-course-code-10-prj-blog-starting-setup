b92ac12ced098e0952718e1c843db70247e604d4
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-09-30 19:52:20 +0200

    Refactor package dependencies and add MongoDB integration for contact form submission

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -14,9 +16,30 @@ const handler = (req, res) => {
       message,
     };
 
-    console.log(newMessage);
+    let client;
 
-    res.status(201).json({ message: 'Successfully stored message!' });
+    try {
+      client = await MongoClient.connect(
+        `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.7ztaz.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0`
+      );
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({ message: 'Successfully stored message!', message: newMessage });
   }
 };
 
