f06fda84b48b197513519e35683f345b5e36371d
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-09-30 20:35:57 +0200

    Add meta tags and improve SEO on _document.js

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="description" content="I post about tennis and web development." />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
