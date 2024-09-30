0b2b693ef40ef688e60dd6baf75b967a72fd86ad
alfonsojim1897 <alfonsojim1897@gmail.com>
2024-09-30 20:32:46 +0200

    Add meta tags and improve SEO on homepage

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
 import { getFeaturedPosts } from '../lib/post-utils';
+import Head from 'next/head';
 
 const Homepage = (props) => {
   return (
     <Fragment>
+      <Head>
+        <title>Poncho's Blog</title>
+        <meta name="description" content="I post about tennis and web development." />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </Fragment>
